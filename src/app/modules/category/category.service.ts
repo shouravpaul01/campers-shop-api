9359a05fdf373153f56a49e403bd65852d5fb617
Shourav Paul <paul.shourav01@gmail.com>
2024-07-11 02:45:37 +0600

    Search,Filter,Paginate,Sort

--- src/app/modules/category/category.service.ts
+++ src/app/modules/category/category.service.ts
@@ -1,43 +1,57 @@
 import httpStatus from "http-status";
 import { Category } from "./category.model";
 import { TCategory } from "./category.interface";
+import { QueryBuilder } from "../../builder/QueryBuilder";
 
 const createCategoryIntoDB = async (payload: TCategory) => {
-    const result = await Category.create(payload);
-    return result;
-  };
-  const getAllCategoriesDB = async (query: Record<string, unknown>) => {
-    const result = await Category.find({});
-    return result;
-  };
-  const getSingleCategoryDB = async (categoryId: string) => {
+  const result = await Category.create(payload);
+  return result;
+};
+const getAllCategoriesDB = async (query: Record<string, unknown>) => {
+ const searchableFields=["name"]
+  const mainQuery = new QueryBuilder(Category.find({}), query).search(
+    searchableFields
+  );
+
+  const totalPages = (await mainQuery.totalPages()).totalQuery;
+  const categoryQuery = mainQuery.paginate();
+  const categories = await categoryQuery.modelQuery;
+
+  const result = { data: categories, totalPages: totalPages };
+  return result;
+};
+const getSingleCategoryDB = async (
+  categoryId: string
+) => {
     const result = await Category.findById(categoryId);
     return result;
-  };
-  const updateCategoryIntoDB = async (categoryId: string, payload: Partial<TCategory>) => {
-    const result = await Category.findByIdAndUpdate(categoryId, payload, {
-      new: true,
-    });
-    return result;
-  };
-  const updateCategoryStatusDB = async (categoryId: string, status: string) => {
-    const result = await Category.findByIdAndUpdate(
-      categoryId,
-      { status: status },
-      { new: true }
-    );
-    return result;
-  };
-  const getAllActiveCategoriesDB = async () => {
-    const result = await Category.find({status:true})
-    return result;
-  };
-  export const CategoryServices = {
-    createCategoryIntoDB,
-    getAllCategoriesDB,
-    getSingleCategoryDB,
-    updateCategoryIntoDB,
-    updateCategoryStatusDB,
-    getAllActiveCategoriesDB
-  };
-  
+};
+const updateCategoryIntoDB = async (
+  categoryId: string,
+  payload: Partial<TCategory>
+) => {
+  const result = await Category.findByIdAndUpdate(categoryId, payload, {
+    new: true,
+  });
+  return result;
+};
+const updateCategoryStatusDB = async (categoryId: string, status: string) => {
+  const result = await Category.findByIdAndUpdate(
+    categoryId,
+    { status: status },
+    { new: true }
+  );
+  return result;
+};
+const getAllActiveCategoriesDB = async () => {
+  const result = await Category.find({ status: true });
+  return result;
+};
+export const CategoryServices = {
+  createCategoryIntoDB,
+  getAllCategoriesDB,
+  getSingleCategoryDB,
+  updateCategoryIntoDB,
+  updateCategoryStatusDB,
+  getAllActiveCategoriesDB,
+};
